b47b27287e47e7e505aa2d073f95810851008b74
Mészáros Gergely <grigorijat9026@gmail.com>
2018-05-31 22:30:22 +0200

    Statistics endpoint and controller implementation

---
+++ src/main/java/com/example/microservices/StatisticsMicroservice/controller/impl/StatisticsController.java
@@ -0,0 +1,27 @@
+package com.example.microservices.StatisticsMicroservice.controller.impl;
+
+import com.example.microservices.StatisticsMicroservice.controller.interfaces.StatisticsEndpoint;
+import com.example.microservices.StatisticsMicroservice.service.StatisticsService;
+import com.example.microservices.StatisticsMicroservice.utilities.JsonResponseBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatisticsController implements StatisticsEndpoint {
+
+    @Autowired
+    private StatisticsService statisticsService;
+
+    @Override
+    public String test() {
+        return "Statistics microservice is available";
+    }
+
+    @Override
+    public ResponseEntity<JsonResponseBody> getStatistics(String jwt, String email) {
+        return ResponseEntity.ok(new JsonResponseBody(HttpStatus.OK.value(), statisticsService.getStatistics(jwt, email)));
+    }
+
+}
---
+++ src/main/java/com/example/microservices/StatisticsMicroservice/controller/interfaces/StatisticsEndpoint.java
@@ -0,0 +1,16 @@
+package com.example.microservices.StatisticsMicroservice.controller.interfaces;
+
+import com.example.microservices.StatisticsMicroservice.utilities.JsonResponseBody;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public interface StatisticsEndpoint {
+
+    @RequestMapping("/test")
+    String test();
+
+    @RequestMapping("/getStatistics")
+    ResponseEntity<JsonResponseBody> getStatistics(@RequestParam(value = "jwt") String jwt, @RequestParam(value = "email") String email);
+
+}
