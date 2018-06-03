b01f3eaae040ca60fb8092c26977a92c76f9d97a
Meszaros Gergely <grigorijat9026@gmail.com>
2018-06-03 22:30:35 +0200

    minor modifications

--- additionalResources/fill_table.sql
+++ additionalResources/fill_table.sql
@@ -1,4 +1,5 @@
 INSERT INTO `latest_statistics` (`ID`, `DESCRIPTION`, `DATE`, `EMAIL`) VALUES
-(1, 'You have <b>3 low priority </b> ToDos and <b>2 high priority</b> ToDos.', '2017-11-11', 'some.email@provider.com'),
-(2, 'You have <b>4 low priority </b> ToDos and <b>1 high priority</b> ToDos.', '2017-11-12', 'some.email@provider.com'),
-(3, 'You have <b>2 low priority</b> ToDos and <b>0 high priority</b> ToDo', '2017-11-20', 'otheremail@someotherprovider.com');
+(1, 'You have <b>3 low priority </b> ToDos and <b>2 high priority</b> ToDo.', '2017-11-11', 'some.email@provider.com'),
+(2, 'You have <b>4 low priority </b> ToDos and <b>1 high priority</b> ToDo.', '2017-11-12', 'some.email@provider.com'),
+(3, 'You have <b>2 low priority</b> ToDos and <b>0 high priority</b> ToDo', '2017-11-20', 'wicked@dummy.eu'),
+(4, 'You have <b>0 low priority</b> ToDos and <b>1 high priority</b> ToDo', '2017-12-24', 'otheremail@someotherprovider.com');
