762dd91272c4cf24519602627e63229bb6e21d78
Mészáros Gergely <grigorijat9026@gmail.com>
2018-06-03 09:42:27 +0200

    sql to set default data into latest_statistics table

---
+++ additionalResources/fill_table.sql
@@ -0,0 +1,4 @@
+INSERT INTO `latest_statistics` (`ID`, `DESCRIPTION`, `DATE`, `EMAIL`) VALUES
+(1, 'You have <b>3 low priority </b> ToDos and <b>2 high priority</b> ToDos.', '2017-11-11', 'some.email@provider.com'),
+(2, 'You have <b>4 low priority </b> ToDos and <b>1 high priority</b> ToDos.', '2017-11-12', 'some.email@provider.com'),
+(3, 'You have <b>2 low priority</b> ToDos and <b>0 high priority</b> ToDo', '2017-11-20', 'otheremail@someotherprovider.com');
