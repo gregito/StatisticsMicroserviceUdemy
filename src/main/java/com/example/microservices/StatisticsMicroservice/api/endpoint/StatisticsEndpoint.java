3c20afa8d5277e040379f4499273c0a08ab13895
Mészáros Gergely <grigorijat9026@gmail.com>
2018-06-05 00:26:39 +0200

    request related annotations moved from endpoint interface(s) to controller layer(s) since PathVariable cannot be passed down through interface solution

--- src/main/java/com/example/microservices/StatisticsMicroservice/api/endpoint/StatisticsEndpoint.java
+++ src/main/java/com/example/microservices/StatisticsMicroservice/api/endpoint/StatisticsEndpoint.java
@@ -2,17 +2,11 @@ package com.example.microservices.StatisticsMicroservice.api.endpoint;
 
 import com.example.microservices.StatisticsMicroservice.utilities.JsonResponseBody;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 public interface StatisticsEndpoint {
 
-    @RequestMapping("/test")
     String test();
 
-    @CrossOrigin
-    @RequestMapping("/getStatistics")
-    ResponseEntity<JsonResponseBody> getStatistics(@RequestParam(value = "jwt") String jwt, @RequestParam(value = "email") String email);
+    ResponseEntity<JsonResponseBody> getStatistics(String jwt, String email);
 
 }
--- src/main/java/com/example/microservices/StatisticsMicroservice/controller/StatisticsController.java
+++ src/main/java/com/example/microservices/StatisticsMicroservice/controller/StatisticsController.java
@@ -6,6 +6,9 @@ import com.example.microservices.StatisticsMicroservice.utilities.JsonResponseBo
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,12 +18,15 @@ public class StatisticsController implements StatisticsEndpoint {
     private StatisticsService statisticsService;
 
     @Override
+    @RequestMapping("/test")
     public String test() {
         return "Statistics microservice is available";
     }
 
     @Override
-    public ResponseEntity<JsonResponseBody> getStatistics(String jwt, String email) {
+    @CrossOrigin
+    @RequestMapping("/getStatistics")
+    public ResponseEntity<JsonResponseBody> getStatistics(@RequestParam(value = "jwt") String jwt, @RequestParam(value = "email") String email) {
         return ResponseEntity.ok(new JsonResponseBody(HttpStatus.OK.value(), statisticsService.getStatistics(jwt, email)));
     }
 
